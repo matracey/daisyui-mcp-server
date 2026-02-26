0ef62ca8b311266af6dba11525bcfcc8360e3458
Martin Tracey <matracey@outlook.ie>
2026-02-25 20:47:46 -0800

    feat: add constants

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const DAISYUI_DOCS_BASE = 'https://daisyui.com/components'
+export const REACT_DAISYUI_REPO = 'https://github.com/daisyui/react-daisyui'
+export const REACT_DAISYUI_SRC_BASE = `${REACT_DAISYUI_REPO}/blob/main/src`
+export const SERVER_NAME = 'daisyui-mcp-server'
+export const SERVER_VERSION = '0.1.0'
+export const MAX_SEARCH_RESULTS = 10
+export const CATEGORIES = [
+  'actions',
+  'data-display',
+  'navigation',
+  'feedback',
+  'layout',
+  'data-input',
+  'mockup',
+] as const
