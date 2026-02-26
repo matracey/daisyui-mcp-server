098a93c6d84ec4cd9f5c5855d24b948b2631b7b3
Martin Tracey <matracey@outlook.ie>
2026-02-25 20:49:22 -0800

    feat: generate initial knowledge base from react-daisyui source

---
+++ src/data/generated/index.ts
@@ -0,0 +1,5 @@
+import data from './components.json' with { type: 'json' }
+
+import type { DaisyUIComponent } from '../../types.js'
+
+export const components: DaisyUIComponent[] = data as DaisyUIComponent[]
