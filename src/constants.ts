68305f7762f9e36b955c50e5d27d4b0668e6ee05
Martin Tracey <matracey@outlook.ie>
2026-02-26 00:58:42 -0800

    chore: bump version to 5.5.19

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const DAISYUI_DOCS_BASE = 'https://daisyui.com/components'
 export const REACT_DAISYUI_REPO = 'https://github.com/daisyui/react-daisyui'
 export const REACT_DAISYUI_SRC_BASE = `${REACT_DAISYUI_REPO}/blob/main/src`
 export const SERVER_NAME = 'daisyui-mcp-server'
-export const SERVER_VERSION = '0.1.0'
+export const SERVER_VERSION = '5.5.19'
 export const MAX_SEARCH_RESULTS = 10
 export const CATEGORIES = [
   'actions',
